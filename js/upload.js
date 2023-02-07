02d5bd649483751ccff41c34cdef4a24e02fc7e2
Touchpong <touchpong.c@ku.th>
2023-02-07 14:34:33 +0700

    แก้ไข js ตอนอัพรูปภาพ

--- js/upload.js
+++ js/upload.js
@@ -3,8 +3,8 @@ function preview_image(event)
      var reader = new FileReader();
      reader.onload = function()
      {
-          var output = document.getElementById('showimg');
-          output.src = reader.result;
+               var output = document.getElementById('showimg');
+               output.src = reader.result;
      }
      reader.readAsDataURL(event.target.files[0]);
 }
